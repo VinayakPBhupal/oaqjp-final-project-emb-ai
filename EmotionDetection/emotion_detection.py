d1b18834d16230639091e2e4913a3f5a23edbf63
Vinayak P Bhupal <77460614+VinayakPBhupal@users.noreply.github.com>
2025-04-19 00:27:26 +0530

    Updated emotion_detection.py
    This commit includes the additional error handling capabilities

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,10 +1,34 @@
+#importing the required modules
 import requests
 
+#function for emotion detection
 def emotion_detector(text_to_analyse):
+    #initializing the url,header, input which will be sent 
+    #as arguments in the post request.
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input = { "raw_document": { "text": text_to_analyse } }
+
+    #post request
     response = requests.post(url, json = input, headers = header)
+
+    #finding the status code
+    status = response.status_code
+
+    #if status code is 400 the dictionary is sent in as None for all the keys
+    if status == 400:
+        formatted_response = { 
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+        return formatted_response
+    
+    #if the status code is other than 400 following linesof code are executed
+    #for formatting and returning the formatted dictionary
     response = response.json()
     formatted_response = {'emotion':i['emotion'] for y in response.values() for i in y if isinstance(i,dict) if 'emotion' in i.keys() }
     formatted_response = dict(formatted_response['emotion'])
@@ -12,4 +36,4 @@ def emotion_detector(text_to_analyse):
         if j == max(formatted_response.values()):
             formatted_response['dominant_emotion'] = i
             break
-    return formatted_response        
+    return formatted_response  
